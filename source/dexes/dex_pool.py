0cac97f22e2fd6107ffe5c528e8af58e8cf9b9e5
Enottt20 <levcorp03@gmail.com>
2024-07-11 14:54:49 +0300

    DexPool

---
+++ source/dexes/dex_pool.py
@@ -0,0 +1,38 @@
+
+class DexPool:
+    def __init__(
+        self,
+        token0_name,
+        token1_name,
+        token0_address,
+        token1_address,
+        contract_address,
+        web3,
+        contract,
+    ):
+        self.token0_name = token0_name
+        self.token1_name = token1_name
+        self.token0_address = token0_address
+        self.token1_address = token1_address
+        self.contract_address = contract_address
+        self.web3 = web3
+        self.contract = contract
+
+    async def fee(self):
+        fee = await self.contract.functions.fee().call()
+        return fee
+
+    async def liquidity(self):
+        liquidity = await self.contract.functions.liquidity().call()
+        return liquidity
+
+    async def get_token0_price(self):
+        slot0 = await self.contract.functions.slot0().call()
+        price = (slot0[0] / 2**96) ** 2
+        return price
+
+    async def get_token1_price(self):
+        slot0 = await self.contract.functions.slot0().call()
+        price = (slot0[0] / 2**96) ** 2
+        price = price**-1
+        return price
