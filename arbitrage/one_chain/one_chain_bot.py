32e60e10a4a449f6e71729e4c6339fccdb8b906a
123 <123123!@sobaka>
2024-07-25 12:57:53 +0300

    first work commit

--- arbitrage/one_chain/one_chain_bot.py
+++ arbitrage/one_chain/one_chain_bot.py
@@ -1,6 +1,16 @@
 import asyncio
-from source.logger import arbitrage_one_chain_bot_logger
-from source.dexes import pancakeswap, quickswap, uniswapV2
+from constants import BSC
+from source.logger import one_chain_bot_logger
+from source.dexes.dex_class import DexClass
+from source.dexes.dex_pool import DexPool
+from source.dexes.pancakeswap.v3.pancakeswapV3 import PancakeSwapV3, PancakeSwapV3Pool
+from source.dexes.uniswap.v3.uniswapV3 import UniSwapV3, UniSwapV3Pool
+# from source.dexes.quickswap.v3.quickswapV3 import QuickSwap, QuickSwapV3Pool
+from service_settings import (
+    PRIVATE_KEY,
+    FERNET_CRYPT_KEY,
+)
+from source.abi_storage import AbiStorage, Net, SmartContractName
 
 #todo константы
 
@@ -8,49 +18,78 @@ class OneChainBot:
 
     def set_pool_settings(self):
 
-        network, asset, dexes_list, basic_asset = magick_get_settings()
+        network = Net.BNB.value
 
-        if basic_asset == None:
-            self.basic_asset_address = const.USDT_ADDRESS
-        else:
-            self.basic_asset_address = basic_asset
+        dexes_list = BSC.DEX_LIST
 
-        self.asset_address = asset
+        self.fee = int(BSC.FEE)
+
+        self.basic_asset_address = BSC.USDT
+
+        self.asset_address = BSC.USDC
 
         return network, dexes_list
 
-    def initialize_dex(self, network: str, dex_name: str) -> DexClass:
-        if network == "Polygon":
+    def initialize_dex(self, network: str, dex_name: str, abi_storage: AbiStorage) -> DexClass:
+        if network == Net.BNB.value:
+
             if dex_name == "PancakeSwap":
-                dex = pancakeswap.PancakeSwapV3(
-                node_url=const.POLYGON_NODE_URL,
-                private_key=const.PRIVATE_KEY,
-                fernet_key=const.FERNET_CRYPT_KEY,
-                router_address=const.PANCAKE_SWAP_ROUTER,
-                factory_address=const.PANCAKE_SWAP_FACTORY,
-            )
+
+                abi_factory = abi_storage.get_abi(
+                    Net.BNB.value, SmartContractName.PancakeSwapFactory.value
+                )
+                abi_router = abi_storage.get_abi(
+                    Net.BNB.value, SmartContractName.PancakeSwapRouter.value
+                )
+                abi_pool = abi_storage.get_abi(
+                    Net.BNB.value, SmartContractName.PancakeSwapV3Pool.value
+                )
+
+                dex = PancakeSwapV3(
+                    node_url="https://bsc-dataseed1.binance.org/",
+                    private_key=PRIVATE_KEY,
+                    fernet_key=FERNET_CRYPT_KEY,
+                    router_address=BSC.PANCAKE_SWAP_ROUTER,
+                    factory_address=BSC.PANCAKE_SWAP_FACTORY,
+                    abi_factory=abi_factory,
+                    abi_router=abi_router,
+                    abi_pool=abi_pool,
+                )
+
             if dex_name == "Uniswap":
-                dex = uniswapV2.UniSwapV2(
-                node_url=const.POLYGON_NODE_URL,
-                private_key=const.PRIVATE_KEY,
-                fernet_key=const.FERNET_CRYPT_KEY,
-                router_address=const.UNISWAP_ROUTER,
-                factory_address=const.UNISWAP_FACTORY,
-            )
-            if dex_name == "Quickswap":
-                dex = quickswap.QickSwap(
-                node_url=const.POLYGON_NODE_URL,
-                private_key=const.PRIVATE_KEY,
-                fernet_key=const.FERNET_CRYPT_KEY,
-                router_address=const.QUICKSWAP_ROUTER,
-                factory_address=const.QUICKSWAP_FACTORY,
-            )
+
+                abi_factory = abi_storage.get_abi(
+                    Net.BNB.value, SmartContractName.UniswapFactory.value
+                )
+                abi_router = abi_storage.get_abi(
+                    Net.BNB.value, SmartContractName.UniswapRouter.value
+                )
+                abi_pool = abi_storage.get_abi(Net.BNB.value, SmartContractName.UniswapPool.value)
+
+                dex = UniSwapV3(
+                    node_url="https://bsc-dataseed1.binance.org/",
+                    private_key=PRIVATE_KEY,
+                    fernet_key=FERNET_CRYPT_KEY,
+                    router_address=BSC.UNISWAP_ROUTER,
+                    factory_address=BSC.UNISWAP_FACTORY,
+                    abi_factory=abi_factory,
+                    abi_router=abi_router,
+                    abi_pool=abi_pool,
+                )
+            # if dex_name == "Quickswap":
+            #     dex = QuickSwap(
+            #         node_url="https://bsc-dataseed1.binance.org/",
+            #         private_key=const.PRIVATE_KEY,
+            #         fernet_key=const.FERNET_CRYPT_KEY,
+            #         router_address=const.QUICKSWAP_ROUTER,
+            #         factory_address=const.QUICKSWAP_FACTORY,
+            # )
             else:
                 raise ValueError(f"Invalid DEX name: {dex_name}")
         return dex
 
-    def create_pool(self, dex) -> DexPool:
-        await dex.get_pool(
+    def create_pool(self, dex: DexClass) -> DexPool:
+        pool = await dex.get_pool(
             first_address=self.asset_address,
             second_address=self.basic_asset_address,
             fee=self.fee
@@ -58,18 +97,19 @@ class OneChainBot:
         return pool
 
     def set_tradings_variables(self, network, dexes_list):
-        # todo реализовать получение pair
-        pair = config.pairs.get(BSC.PAIR)
-        self.fee = int(pair.get("fee"))
+
+        abi_storage = AbiStorage()
+
+        self.basic_asset_abi = abi_storage.get_abi(Net.BNB.value, SmartContractName.USDT.value)
+        self.asset_abi = abi_storage.get_abi(Net.BNB.value, SmartContractName.USDC.value)
 
         self.first_dex = None
-
         for dex_name in dexes_list:
             if self.first_dex == None:
-                self.first_dex = self.initialize_dex(network, dex_name)
+                self.first_dex = self.initialize_dex(network, dex_name, abi_storage)
                 self.first_pool = self.create_pool(self.first_dex)
             else:
-                self.second_dex = self.initialize_dex(network, dex_name)
+                self.second_dex = self.initialize_dex(network, dex_name, abi_storage)
                 self.second_pool = self.create_pool(self.second_dex)
 
     def work(self):
@@ -82,50 +122,46 @@ class OneChainBot:
 
             elif first_pool_price < second_pool_price:
                 await self.first_dex.swap(
-                    to_address=PUBLIC_KEY,
                     amount_in=await self.first_dex.web3.to_wei(1, "ether"),
                     amount_out=await self.first_dex.web3.to_wei(0.9, "ether"),
                     token_in=self.basic_asset_address,
                     token_out=self.asset_address,
-                    abi_token_in=ERC20_abi,
-                    abi_token_out=ERC20_abi,
+                    abi_token_in=self.basic_asset_abi,
+                    abi_token_out=self.asset_abi,
                     pool_fee=self.fee
                 )
                 await self.second_dex.swap(
-                    to_address=PUBLIC_KEY,
                     amount_in=await self.second_dex.web3.to_wei(1, "ether"),
                     amount_out=await self.second_dex.web3.to_wei(0.9, "ether"),
                     token_in=self.asset_address,
                     token_out=self.basic_asset_address,
-                    abi_token_in=ERC20_abi,
-                    abi_token_out=ERC20_abi,
+                    abi_token_in=self.asset_abi,
+                    abi_token_out=self.basic_asset_abi,
                     pool_fee=self.fee
                 )
 
             elif first_pool_price > second_pool_price:
                 await self.second_dex.swap(
-                    to_address=PUBLIC_KEY,
                     amount_in=await self.second_dex.web3.to_wei(1, "ether"),
                     amount_out=await self.second_dex.web3.to_wei(0.9, "ether"),
                     token_in=self.basic_asset_address,
                     token_out=self.asset_address,
-                    abi_token_in=ERC20_abi,
-                    abi_token_out=ERC20_abi,
+                    abi_token_in=self.basic_asset_abi,
+                    abi_token_out=self.asset_abi,
                     pool_fee=self.fee
                 )
                 await self.first_dex.swap(
-                    to_address=PUBLIC_KEY,
                     amount_in=await self.first_dex.web3.to_wei(1, "ether"),
                     amount_out=await self.first_dex.web3.to_wei(0.9, "ether"),
                     token_in=self.asset_address,
                     token_out=self.basic_asset_address,
-                    abi_token_in=ERC20_abi,
-                    abi_token_out=ERC20_abi,
+                    abi_token_in=self.asset_abi,
+                    abi_token_out=self.basic_asset_abi,
                     pool_fee=self.fee
                 )
 
         except Exeption as e:
-            arbitrage_one_chain_bot_logger.error(f"Error accrued- {str(e)}")
+            one_chain_bot_logger.error(f"Error accrued- {str(e)}")
 
 def main():
     try:
@@ -141,4 +177,4 @@ def main():
             OneChainBot.work()
 
     except Exeption as e:
-        arbitrage_one_chain_bot_logger.exeption(f"One chain bot error- {str(e)}")
+        one_chain_bot_logger.exeption(f"One chain bot error- {str(e)}")
